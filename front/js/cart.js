07a3c543106e27c80f730e8b0bc21596111aa637
Thomas Guyot <thomas.guyot@outlook.com>
2022-12-09 12:06:00 +0100

    totaux dynamiques des articles et prix

--- front/js/cart.js
+++ front/js/cart.js
@@ -13,6 +13,10 @@ function getCart(){
 
 let cart = getCart();
 let kanapData = JSON.parse(sessionStorage.getItem("productData"));
+let qtyTotal = document.getElementById('totalQuantity');
+let prixTotal = document.getElementById('totalPrice');
+let kanapPrice = []
+
 
 // affichage des elements dans le DOM
 for (let x = 0; x < cart.length; x++) {
@@ -39,49 +43,72 @@ for (let x = 0; x < cart.length; x++) {
             </div>
             </div>
         </div>
-        </article>`)}
+        </article>`)
+        kanapPrice.push(kanapData[y].price)
+    }
     }
 }
 
 // ecouter les changement de quantité 
 let checkChange = document.querySelectorAll(".itemQuantity");
 checkChange.forEach( elem => {
-    elem.addEventListener("input", 
-    function() {
+    elem.addEventListener("input", () => {
         let cart = getCart()
         tempCart = { 
             ProductID: elem.closest('article').dataset.id,
             color: elem.closest('article').dataset.color,
             qty: parseInt(elem.closest('input').value)
-            }
+        }
     for (let x = 0; x < cart.length; x++) {
         if (cart[x].ProductID === elem.closest('article').dataset.id && cart[x].color === elem.closest('article').dataset.color) {
-            cart.splice(x, 1, tempCart)
-            localStorage.setItem('Panier', JSON.stringify(cart))
-            }}
-        }
-    )
+            cart.splice(x, 1, tempCart);
+            localStorage.setItem('Panier', JSON.stringify(cart));
+        }}
+    })
 })
 
 // suppression d'un element + refresh de la page
 let boutonDelete = document.querySelectorAll(".deleteItem");
 for (let i = 0; i < boutonDelete.length; i++) {
-    boutonDelete[i].addEventListener("click", function() {
-        for (let x = 0; x < cart.length; x++) {
-            if (cart[x].ProductID === boutonDelete[i].closest('article').dataset.id && cart[x].color === boutonDelete[i].closest('article').dataset.color) {
-                let isExecuted = confirm("Voulez vous supprimer cet article du panier ?");
-                if (isExecuted === true) {
-                    cart.splice(x, 1)
-                    localStorage.setItem('Panier', JSON.stringify(cart))
-                    location.reload()}
-                else {
-                    return
-                }
-            }
+    boutonDelete[i].addEventListener("click", () => {
+    for (let x = 0; x < cart.length; x++) {
+        if (cart[x].ProductID === boutonDelete[i].closest('article').dataset.id && cart[x].color === boutonDelete[i].closest('article').dataset.color) {
+            let isExecuted = confirm("Voulez vous supprimer cet article du panier ?");
+            if (isExecuted === true) {
+                cart.splice(x, 1)
+                localStorage.setItem('Panier', JSON.stringify(cart))
+                location.reload()
+            };}
         }
-    })
+})}
+
+// calcul du total d'items
+let articleTotal = () => {
+    let total = 0
+    let cart = getCart();
+    for (let i = 0; i < cart.length; i++) {
+        total = total + cart[i].qty}
+    return total
 }
 
-//               <p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice"><!-- 84,00 --></span> €</p>
+// calcul du total du prix
+let totalPrix = () => {
+    let cart = getCart();
+    let total = 0
+    for (let i = 0; i < kanapPrice.length; i++) {
+        total = total + (kanapPrice[i] * cart[i].qty)}
+    return total
+}
 
+let newQty = document.querySelectorAll(".itemQuantity");
+for (let i = 0; i < newQty.length; i++) {
+    newQty[i].addEventListener("change", () => {
+        qtyTotal.innerText = articleTotal();
+        prixTotal.innerText = totalPrix();
+    }
+    )
+}
+
+qtyTotal.innerText = articleTotal()
+prixTotal.innerText = totalPrix()
 
