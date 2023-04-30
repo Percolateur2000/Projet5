b5e6123ed8d548736ade20e8cd4c9b754811db96
Vaën <vaentaf@gmail.com>
2023-04-30 20:14:28 +0200

    Update script.js
    version offline

--- front/js/script.js
+++ front/js/script.js
@@ -2,7 +2,9 @@ let i = 0;
 
 // debut de la fonction une fois que le DOM est chargé
 let start = () => {
-  fetch("http://localhost:3000/api/products")
+  // enlever les commentaires pour utiliser l'api en local
+  //  fetch("http://localhost:3000/api/products")
+  fetch("https://kanapi.gtnsimon.dev/api/products")
     .then((res) => res.json())
     .then((data) => {
       sessionStorage.setItem("productData", JSON.stringify(data));
